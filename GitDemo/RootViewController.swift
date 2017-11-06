f9f9366c369c9fa0adac687e57964083a6e18f88
E <e@Ernestos-MacBook-Pro.local>
2017-11-06 10:50:14 -0500

    Initial Commit

---
+++ GitDemo/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  GitDemo
+//
+//  Created by Ernesto Bautista on 11/6/17.
+//  Copyright © 2017 Immerzion Interactive. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
